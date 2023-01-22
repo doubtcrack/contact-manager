e6ac88adb73fabdc65c2e79d1f415e48da2fd744
doubtcrack <1.edujock@gmail.com>
2023-01-22 15:33:59 +0530

    contact-manager

--- src/Charts/components/CountryPicker/CountryPicker.jsx
+++ src/Charts/components/CountryPicker/CountryPicker.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
-import { NativeSelect, FormControl } from "@material-ui/core";
+// import { NativeSelect, FormControl } from "@material-ui/core";
 
-import { fetchCountries } from "../../api";
+// import { fetchCountries } from "../../api";
 
 const Countries = ({ handleCountryChange }) => {
   const [countries, setCountries] = useState([
@@ -14,7 +14,7 @@ const Countries = ({ handleCountryChange }) => {
   ]);
   // console.log(countries);
   // useEffect(() => {
-  //   const fetchAPI = async () => {
+  //   const fetchAPI = async () => {s
   //     setCountries(await fetchCountries());
   //   };
 
@@ -22,19 +22,20 @@ const Countries = ({ handleCountryChange }) => {
   // }, []);
   console.log(countries);
   return (
-    <FormControl className="w-2/5 mb-3">
-      <NativeSelect
-        defaultValue=""
-        onChange={(e) => handleCountryChange(e.target.value)}
-      >
-        <option value="">United States</option>
-        {countries.map((country, i) => (
-          <option key={i} value={country}>
-            {country}
-          </option>
-        ))}
-      </NativeSelect>
-    </FormControl>
+    // <FormControl className="w-2/5 mb-3">
+    //   <NativeSelect
+    //     defaultValue=""
+    //     onChange={(e) => handleCountryChange(e.target.value)}
+    //   >
+    //     <option value="">United States</option>
+    //     {countries.map((country, i) => (
+    //       <option key={i} value={country}>
+    //         {country}
+    //       </option>
+    //     ))}
+    //   </NativeSelect>
+    // </FormControl>
+    <>CountryPicker</>
   );
 };
 
